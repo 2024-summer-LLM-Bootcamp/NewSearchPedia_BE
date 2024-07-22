323e92ab5f26b3bd66619f4222b7106bef4dd321
haram8009 <haram8009@gmail.com>
2024-07-22 17:48:58 +0900

    custom user model 적용

--- newsearchpedia_be/settings/base.py
+++ newsearchpedia_be/settings/base.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # my app
+    'accounts',
+    'articles',
+
     # django rest_framework
     'rest_framework',
     'rest_framework.authtoken',
@@ -149,6 +153,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
