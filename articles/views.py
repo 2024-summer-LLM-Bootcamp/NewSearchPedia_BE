77ce7cfe3d24c2d96989c2e342757846e971040f
suyeon1104 <suyeon1104@gmail.com>
2024-07-24 07:24:49 +0000

    paging

--- articles/views.py
+++ articles/views.py
@@ -3,12 +3,16 @@ from django.http import httpResponse
 from .models import Article
 from datetime import timezone
 import articles
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
+    page = request.GET.get('page', '1')
     article_list = Article.objects.order.by('-create_date')
-    context = {'article_list': article_list}
+    paginator = Paginator(article_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'article_list': page_obj}
     # rendering html
     return render(request, 'articles/article_list.html', context)
 def detail(request, article_id):
