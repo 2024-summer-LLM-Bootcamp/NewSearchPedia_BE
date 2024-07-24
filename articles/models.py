248c6e54a74ca194592e2828b8c8813140fafc9f
suyeon1104 <suyeon1104@gmail.com>
2024-07-24 05:09:17 +0000

    modles

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    article_id = models.IntegerField()
+    user_id = models.IntegerField()
+    user_input = models.TextField()
+    news_summary = models.TextField()
+    created_at = models.DateTimeField()
+
+
+class news(models.Model):
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    title = models.charField(max_Length=100)
+    content = models.TextField()
+    link = models.URLField()
+    thumbnail = models.ImageField
+    date = models.DateTimeField()
+
+class encyc(models.Models):
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    keyword =  models.CharField(max_length=30)
+    content = models.TextField()
+    encyc_link = models.URLField()
