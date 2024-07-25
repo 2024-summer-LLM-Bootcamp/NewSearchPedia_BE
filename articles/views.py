9a17ec7771c76998dc661e4234693063cd3f90a0
haram8009 <haram8009@gmail.com>
2024-07-25 16:49:57 +0900

    feat: pagination 적용

--- articles/views.py
+++ articles/views.py
@@ -14,20 +14,50 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 '''
+
+
 class ArticlePagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
 
+
 class Article_View(APIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     pagination_class = ArticlePagination
+
+    @property
+    def paginator(self):
+        if not hasattr(self, '_paginator'):
+            if self.pagination_class is None:
+                self._paginator = None
+            else:
+                self._paginator = self.pagination_class()
+        else:
+            pass
+        return self._paginator
+
+    def paginate_queryset(self, queryset):
+        if self.paginator is None:
+            return None
+        return self.paginator.paginate_queryset(queryset,
+                                                self.request, view=self)
+
+    def get_paginated_response(self, data):
+        assert self.paginator is not None
+        return self.paginator.get_paginated_response(data)
+
     def get(self, request):
         if not request.user.is_authenticated:
             return Response({'error': '로그인이 필요합니다!'}, status=status.HTTP_401_UNAUTHORIZED)
         articles = Article.objects.filter(user_id=request.user)
-        serializer = ArticleSerializer(articles, many=True)
+        page = self.paginate_queryset(articles)
+        if page is not None:
+            serializer = self.get_paginated_response(self.serializer_class(page,
+                                                                           many=True).data)
+        else:
+            serializer = self.serializer_class(articles, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
@@ -54,9 +84,10 @@ class Article_View(APIView):
 
         new_article.save()
 
-        serializer = ArticleSerializer(new_article)
+        serializer = self.serializer_class(new_article)
         return Response({'messasge': 'created', 'article': serializer.data}, status=status.HTTP_201_CREATED)
-    
+
+
 class Article_Detail_View(View):
     def get(self, request, article_id):
         # auth
