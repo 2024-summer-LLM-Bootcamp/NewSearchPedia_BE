f93319d699617b760e32ddbbb0f84dd52023fa13
haram8009 <haram8009@gmail.com>
2024-07-25 15:42:37 +0900

    feat: 아티클 상세 엔드포인트 추가

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Article_View.as_view())
+    path('', views.Article_View.as_view()),
+    path('/detail/<int:article_id>', views.Article_Detail_View.as_view())
 ]
