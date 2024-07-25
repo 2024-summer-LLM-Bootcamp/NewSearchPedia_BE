3aca9e7894b6b42898cbda6ba64a3444bcc25287
suyeon1104 <suyeon1104@gmail.com>
2024-07-25 06:53:45 +0000

    paging

--- articles/views.py
+++ articles/views.py
@@ -1,17 +1,23 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import viewsets
 
 from .models import Article
 from .serializers import ArticleSerializer
 from .utils.utils import *
-# Create your views here.
 
+# Create your views here.
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 class Article_View(APIView):
     def get(self, request):
         # TODO: 회원인증 필수
-        if request.user.is_authenticated:
+        if not request.user.is_authenticated:
             return Response({'error': '로그인이 필요합니다!'}, status=status.HTTP_401_UNAUTHORIZED)
         articles = Article.objects.filter(user_id=request.user)
         serializer = ArticleSerializer(articles, many=True)
@@ -19,7 +25,7 @@ class Article_View(APIView):
         
     def post(self, request):
         # TODO: 회원인증 필수
-        if request.user.is_authenticated:
+        if not request.user.is_authenticated:
             return Response({'error': '로그인이 필요합니다!'}, status=status.HTTP_401_UNAUTHORIZED)
         print(data)
         query = data['query']
